9059ef1aeaf94df8da6e5292fe0f04c103f228a2
Ishaan <er.ishaannagpal@gmail.com>
2020-03-11 12:20:24 +0800

    Weather detail repository added in Repository module

--- app/src/main/java/com/spdigital/seekweather/di/RepositoryModule.kt
+++ app/src/main/java/com/spdigital/seekweather/di/RepositoryModule.kt
@@ -2,8 +2,11 @@ package com.spdigital.seekweather.di
 
 
 import com.spdigital.seekweather.repository.LocationRepositoryImpl
+import com.spdigital.seekweather.repository.WeatherDetailRepository
+import com.spdigital.seekweather.repository.WeatherDetailRepositoryImpl
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    single { LocationRepositoryImpl( get()) }
+    single { LocationRepositoryImpl(get()) }
+    single { WeatherDetailRepositoryImpl(get()) }
 }
